611b625265fd090dfb1632c7cbb8b88dffbaccf2
ArcadeZero <ArcadeZero417@gmail.com>
2019-03-30 13:34:37 -0700

    Update BatEEEE.js

--- js/BatEEEE.js
+++ js/BatEEEE.js
@@ -38,5 +38,5 @@ document.addEventListener('keydown', function(e) {
 });
 
 function activateCheats() {
-  document.intro.style.background = "url('https://i.imgur.com/z3ffwWH.png')"
+  document.content.style.background = "url('https://i.imgur.com/z3ffwWH.png')"
 }
