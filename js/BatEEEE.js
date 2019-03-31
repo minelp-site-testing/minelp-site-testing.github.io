b453dfc953c2c7929e9bdd89543364514393dc44
ArcadeZero <ArcadeZero417@gmail.com>
2019-03-31 09:53:55 -0700

    Update BatEEEE.js

--- js/BatEEEE.js
+++ js/BatEEEE.js
@@ -38,5 +38,5 @@ document.addEventListener('keydown', function(e) {
 });
 
 function activateCheats() {
-document.getElementById("intro").style.background = "../assets/easteregg/batbanner.svg')"
+document.getElementById("intro").style.background = "url('https://minelp-site-testing.github.io/assets/easteregg/batbanner.svg')"
 }
